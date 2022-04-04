a5e92a93f58d9b319f2069e96cf81b29c2afaf5a
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-04-04 09:45:49 -0400

    built out models/index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,36 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
 
-module.exports = { User, Project };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post,{
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+
+
+module.exports = { User, Post, Comment };
