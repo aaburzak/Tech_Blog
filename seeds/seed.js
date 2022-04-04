8dbe66c62d6470d960d8680e782a3ef4e0a847f2
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-04-04 10:27:21 -0400

    buit out seeds/seeds.js, created seeds/commentData.json

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+const commentData = require ('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,11 +13,19 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const posts of postData) {
+    await Post.create({
+      ...posts,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
+  }
+
+  for (const comments of commentData) {
+    await Comment.create({
+      ...comments,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
+    })
   }
 
   process.exit(0);
